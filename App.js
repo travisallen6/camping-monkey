a3b820e22bc7ddf2c9d5ed022f4adf1e27d60b43
Travis Allen <travisallen6@gmail.com>
2018-04-25 11:57:05 -0600

    Initial commit on branch

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Button, Alert } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+        <Image source={{uri: "http://http.cat/100"}} style={{width: 200, height: 200}} />
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
+        <Button
+          onPress={() => {
+            Alert.alert('You tapped the button!');
+          }}
+          title="Press Me"
+        />
       </View>
     );
   }
