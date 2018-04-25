18f734fad3040321ef6e02cba0d58e12ca035d05
Travis Allen <travisallen6@gmail.com>
2018-04-25 14:06:24 -0600

    Routes set up

--- App.js
+++ App.js
@@ -1,42 +1,27 @@
 import React from 'react';
-import { StyleSheet, Text, View defaul, Image, Button, Alert } from 'react-native';
-import { Card } from 'react-native-elements';
+import { View, Text } from 'react-native';
+import { StackNavigator } from 'react-navigation'; // Version can be specified in package.json
 
-export default class App extends React.Component
+
+const RootStack = StackNavigator(
+  {
+    Home: {
+      screen: HomeScreen,
+    },
+    Pokemon: {
+      screen: Pokemon,
+    },
+    Starwars: {
+      screen: Starwars,
+    },
+  },
+  {
+    initialRouteName: 'Home',
+  }
+);
+
 export default class App extends React.Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Image source={{uri: "http://http.cat/100"}} style={{width: 200, height: 200}} />
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
- {
-  constructor(){
-    super()
-    this.state = {
-
-    render() {
-    return (
-ew style  <Imag=tyles.contaier}>
- s   e source={{uri:/10http://htt0"}cat  
-omponent {
-    render() {
-    return (
-      <View style=
-    }
-iew style={styles.container}>
-        <Image source={{uri: "http://http.cat/100"}iew style={styles.container}>
-      <Image source={{uri: "http://http.cat/100"}} style={{width: 200, height: 200}} />
-    <Text>Open up App.js to start working on your app!</Text>
-      <Text>Changes you make will automatically reload.</Text>
-      <Text>Shake your phone to open the developer menu.</Text>
-yles.container}>
-        <Image source={{uri: "http://http.cat/100"}} style={{width: 200, height: 200}} />
-        <Text>Type a Pokemon ID Below to Get Data</Text>
-        <Card></Card>
-        <Button
-          onPress={() => {
-            Alert.alert('You tapped the button!');
-          }}
-          title="Press Me"
+    return <RootStack />;
+  }
+}
