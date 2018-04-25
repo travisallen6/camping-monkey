d529b63ea8496c1102460218bd6c6976f5377721
DalThePal <dallen.hoyal@gmail.com>
2018-04-25 12:08:09 -0600

    axios call

--- App.js
+++ App.js
@@ -1,13 +1,45 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, Button, Image, StyleSheet, Text, TextInput, View } from 'react-native';
+import axios from 'axios';
 
 export default class App extends React.Component {
+
+  state = {
+    text: 'useless placeholder',
+    url: 'https://pokeapi.co/api/v2/pokemon/1',
+    pokemon: {}
+  }
+
+  componentDidMount() {
+    axios.get(this.state.url).then(res => this.setState({
+      pokemon: res.data,
+      sprites: res.data.sprites.front_default
+    }))
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+
+        <Image
+          source={{ uri: this.state.sprites }}
+          style={{ width: 400, height: 400 }}
+        />
+
+        <TextInput
+          style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+          onChangeText={(text) => this.setState({ text })}
+          value={this.state.text}
+        />
+
+
+        <Button
+          onPress={() => {
+            Alert.alert(this.state.pokemon.name);
+          }}
+          title="Press Me"
+        />
+
       </View>
     );
   }
