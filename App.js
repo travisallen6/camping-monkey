4938f6fda1bad7b448112867055a27b200313023
DalThePal <dallen.hoyal@gmail.com>
2018-04-25 13:51:19 -0600

    created pokemon component

--- App.js
+++ App.js
@@ -1,60 +1,16 @@
-import React from 'react';
-import { Alert, Button, Image, StyleSheet, Text, TextInput, View } from 'react-native';
-import axios from 'axios';
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+import Pokemon from './Pokemon'
 
-export default class App extends React.Component {
-  constructor(){
+export default class App extends Component {
+  constructor() {
     super()
     
-    this.state = {
-      id: 1,
-      url: 'https://pokeapi.co/api/v2/pokemon/',
-      pokemon: {}
-    }
-  }
-
-  getPokemon() {
-    axios.get(`${this.state.url}${this.state.id}`).then(res => this.setState({
-      pokemon: res.data,
-      sprite: res.data.sprites.front_default
-    }))
   }
 
   render() {
-    return (
-      <View style={styles.container}>
-
-        <Image
-          source={{ uri: this.state.sprite }}
-          style={{ width: 400, height: 400 }}
-        />
-
-        <Text>{this.state.pokemon.name}</Text>
-
-        <TextInput
-          style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
-          onChangeText={(id) => this.setState({ id })}
-          value={'id'}
-        />
-
-
-        <Button
-          onPress={() => {
-            this.getPokemon(this.state.id);
-          }}
-          title="Search"
-        />
-
-      </View>
-    );
+    return(
+      <Pokemon/>
+    )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
