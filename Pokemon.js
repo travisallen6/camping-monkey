600dc1bdad65a45f1855ce573bf2a6e5ec626e18
Travis Allen <travisallen6@gmail.com>
2018-04-25 15:00:19 -0600

    controlled inputs

--- Pokemon.js
+++ Pokemon.js
@@ -34,7 +34,7 @@ export default class Pokemon extends React.Component {
         <TextInput
           style={{ width:100, height: 40, borderColor: 'gray', borderWidth: 1 }}
           onChangeText={(id) => this.setState({ id })}
-          value={''}
+          value={this.state.id}
         />
 
 
