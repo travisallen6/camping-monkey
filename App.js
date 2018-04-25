bbdd7fb5ad1529dd4a84f66e619b649ebe1a2a3d
Travis Allen <travisallen6@gmail.com>
2018-04-25 12:10:59 -0600

    second commit

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, Button, Alert } from 'react-native';
+import { StyleSheet, Text, View defaul, Image, Button, Alert } from 'react-native';
+import { Card } from 'react-native-elements';
 
+export default class App extends React.Component
 export default class App extends React.Component {
   render() {
     return (
@@ -9,22 +11,32 @@ export default class App extends React.Component {
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
+ {
+  constructor(){
+    super()
+    this.state = {
+
+    render() {
+    return (
+ew style  <Imag=tyles.contaier}>
+ s   e source={{uri:/10http://htt0"}cat  
+omponent {
+    render() {
+    return (
+      <View style=
+    }
+iew style={styles.container}>
+        <Image source={{uri: "http://http.cat/100"}iew style={styles.container}>
+      <Image source={{uri: "http://http.cat/100"}} style={{width: 200, height: 200}} />
+    <Text>Open up App.js to start working on your app!</Text>
+      <Text>Changes you make will automatically reload.</Text>
+      <Text>Shake your phone to open the developer menu.</Text>
+yles.container}>
+        <Image source={{uri: "http://http.cat/100"}} style={{width: 200, height: 200}} />
+        <Text>Type a Pokemon ID Below to Get Data</Text>
+        <Card></Card>
         <Button
           onPress={() => {
             Alert.alert('You tapped the button!');
           }}
           title="Press Me"
-        />
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
